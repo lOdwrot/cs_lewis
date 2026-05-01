2f896a8c5ac415f7503be87ad4e0e8ca98560141
Lukasz Odwrot <lukasz.odwrot@dynatrace.com>
2026-05-01 11:35:05 +0200

    Added redirect

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const STRAPI_URL = import.meta.env.VITE_STRAPI_URL ?? 'http://localhost:1337'
+const STRAPI_URL = import.meta.env.VITE_STRAPI_URL || 'http://localhost:1337'
 
 export const api = axios.create({
   baseURL: `${STRAPI_URL}/api`,
